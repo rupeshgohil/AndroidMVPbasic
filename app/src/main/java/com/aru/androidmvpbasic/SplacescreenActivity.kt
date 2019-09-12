a7b38e91ad12ecf29b9348b19aa98926fd42214b
Rupesh Gohil <rupeshgohel007@gmail.com>
2019-09-12 13:23:31 +0530

    add SplaceScreen

---
+++ app/src/main/java/com/aru/androidmvpbasic/SplacescreenActivity.kt
@@ -0,0 +1,17 @@
+package com.aru.androidmvpbasic
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_splacescreen.*
+
+class SplacescreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splacescreen)
+
+    }
+
+}
