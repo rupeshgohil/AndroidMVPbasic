64c33ab6367f4e048752a499c28a7a047605dc18
Rupesh Gohil <rupeshgohel007@gmail.com>
2019-09-12 13:25:16 +0530

    add SplaceScreen

--- app/src/main/java/com/aru/androidmvpbasic/SplacescreenActivity.kt
+++ app/src/main/java/com/aru/androidmvpbasic/SplacescreenActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
-import kotlinx.android.synthetic.main.activity_splacescreen.*
 
 class SplacescreenActivity : AppCompatActivity() {
 
